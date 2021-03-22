509595fb30cc65b2c2b2d0edb9e0b408ce3007c3
mariagabs <71193719+mariagabs@users.noreply.github.com>
2021-03-22 20:19:42 -0300

    Novas telinhas e suas controllers
    +2 .fxml, +2 Controllers, +2 .png, algumas classes novas no .css

--- worke/src/sample/Main.java
+++ worke/src/sample/Main.java
@@ -1,6 +1,7 @@
 package sample;
 
 import javafx.application.Application;
+import javafx.application.Platform;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
@@ -10,10 +11,14 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("login.fxml"));
         primaryStage.setTitle("worke!");
         Scene scene = new Scene(root, 1200, 780);
         scene.getStylesheets().add(getClass().getResource("/resources/fonts/styles.css").toExternalForm());
+
+        /* código que tira o foco dos textfields ao carregar a tela (antes focava e nao aparecia a placeholder */
+        Platform.runLater( () -> root.requestFocus() );
+
         primaryStage.setScene(scene);
         primaryStage.show();
     }
