3511cfac2c4cb9de22bdeb3627ae8492d20bb7fd
mariagabs <71193719+mariagabs@users.noreply.github.com>
2021-03-23 19:18:51 -0300

    começo da navegação entre as telas, imagem nos botoes

--- worke/src/sample/Controller.java
+++ worke/src/sample/Controller.java
@@ -1,12 +1,21 @@
 package sample;
 
+import javafx.animation.Animation;
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
+import javafx.application.Platform;
 import javafx.beans.DefaultProperty;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.chart.PieChart;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
@@ -14,10 +23,19 @@ import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.Pane;
 import javafx.scene.text.Font;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+import javafx.util.Duration;
 
 import java.io.File;
+import java.io.IOException;
 import java.net.URL;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 import java.util.ResourceBundle;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
 
 public class Controller implements Initializable {
 
@@ -26,7 +44,7 @@ public class Controller implements Initializable {
     @FXML
     private ImageView ExerciseDetails;
     @FXML
-    private ImageView playPause;
+    private Button playPause;
     @FXML
     private Pane homeGrayScreen;
     @FXML
@@ -35,21 +53,79 @@ public class Controller implements Initializable {
     private Pane exerciseScreen;
     @FXML
     private GridPane playGrid;
+    @FXML
+    private ImageView Logout;
+    @FXML
+    private ImageView play;
+    @FXML
+    private Label timer;
+    @FXML
+    private Image btnImagePlay;
 
     Image imagePause = new Image(getClass().getResource("/resources/img/simbolo-de-pausa.png").toExternalForm());
     Image imagePlay = new Image(getClass().getResource("/resources/img/botao-play-ponta-de-seta.png").toExternalForm());
 
     static boolean pause;
 
+    private void teste() {
+        ScheduledExecutorService exec = Executors.newScheduledThreadPool(1);
 
+        final int[] secondsToWait = {180};
+        Runnable task = new Runnable() {
+            @Override
+            public void run() {
+                secondsToWait[0]--;
+                timer.setText(secondsToWait[0] + "");
+                if (secondsToWait[0] == 0) {
+                    exec.shutdown();
+                }
+            }
+        };
+
+        timer.setText(secondsToWait[0] + "");
+        exec.scheduleAtFixedRate(task, 1, 1, TimeUnit.SECONDS);
+        
+    }
+
+    EventHandler<ActionEvent> playPauseEvent = new EventHandler<ActionEvent>() {
+        public void handle(ActionEvent e)
+        {
+            if (pause) {
+                btnImagePlay = imagePlay;
+                pause = false;
+                //teste();
+            } else {
+                pause = true;
+                btnImagePlay = imagePause;
+
+            }
+        }
+    };
+    
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
+
+        playPause.setOnAction(playPauseEvent);
+
         Home.setPickOnBounds(true);
         Home.setOnMouseClicked((MouseEvent e) -> {
             homeGrayScreen.setVisible(true);
             homeWhiteScreen.setVisible(true);
             exerciseScreen.setVisible(false);
         });
+
+        play.setPickOnBounds(true);
+        play.setOnMouseClicked((MouseEvent e) -> {
+            if (pause) {
+                btnImagePlay = imagePlay;
+                pause = false;
+            } else {
+                pause = true;
+                btnImagePlay = imagePause;
+
+            }
+        });
+
         ExerciseDetails.setPickOnBounds(true);
         ExerciseDetails.setOnMouseClicked((MouseEvent e) -> {
             exerciseScreen.setVisible(true);
@@ -58,18 +134,24 @@ public class Controller implements Initializable {
         });
 
 
-        playGrid.setOnMouseClicked((MouseEvent e) -> {
+        Logout.setPickOnBounds(true);
+        Logout.setOnMouseClicked((MouseEvent e) -> {
+            try {
+                Scene scene = new Scene(FXMLLoader.load(getClass().getResource("login.fxml")));
+                Stage stage = new Stage();
+                stage.initStyle(StageStyle.UNDECORATED);
+                stage.setScene(scene);
+                stage.show();
 
-            if (pause) {
-                playPause.setImage(imagePlay);
-                pause = false;
-            } else {
-                pause = true;
-                playPause.setImage(imagePause);
-
+                stage = (Stage) Logout.getScene().getWindow();
+                stage.close();
+            } catch (IOException ioException) {
+                ioException.printStackTrace();
             }
-
         });
 
     }
+    
+    
+
 }
--- worke/src/sample/CriarSenhaController.java
+++ worke/src/sample/CriarSenhaController.java
@@ -1,4 +1,44 @@
 package sample;
 
-public class CriarSenhaController {
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.fxml.FXML;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.input.MouseEvent;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class CriarSenhaController implements Initializable {
+
+    @FXML
+    private Button entrar;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+        entrar.setPickOnBounds(true);
+        entrar.setOnMouseClicked((MouseEvent e) -> {
+            Scene scene = null;
+            try {
+                scene = new Scene(FXMLLoader.load(getClass().getResource("sample.fxml")));
+            } catch (IOException ioException) {
+                ioException.printStackTrace();
+            }
+            Stage stage = new Stage();
+            stage.initStyle(StageStyle.UNDECORATED);
+            stage.setScene(scene);
+            stage.show();
+
+            stage = (Stage) entrar.getScene().getWindow();
+            stage.close();
+        });
+
+    }
+
+
 }
